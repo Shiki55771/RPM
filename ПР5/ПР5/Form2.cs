e22333e5065cad7a3de9c83a3729a840e7862193
Shiki55771 <133943355+Shiki55771@users.noreply.github.com>
2023-05-20 08:33:30 +0300

    Add files via upload

---
+++ ПР5/ПР5/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР5
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Form1.BM;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                SaveFileDialog SFD = new SaveFileDialog();
+                SFD.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+                if (SFD.ShowDialog() == DialogResult.OK)
+                {
+                    Form1.BM.Save(SFD.FileName);
+                }           
+
+        }
+    }
+}
