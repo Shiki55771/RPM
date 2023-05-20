947b3a3d33e8505baee53cea8e907647253dead3
Shiki55771 <133943355+Shiki55771@users.noreply.github.com>
2023-05-20 08:34:11 +0300

    Add files via upload

---
+++ ПР 6/ПР 6/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ПР_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonLoadPicture = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonMixed = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonRestore = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonSetting = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonHelp = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonLoadPicture,
+            this.toolStripButtonMixed,
+            this.toolStripButtonRestore,
+            this.toolStripButtonSetting,
+            this.toolStripButtonHelp});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(640, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonLoadPicture
+            // 
+            this.toolStripButtonLoadPicture.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonLoadPicture.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonLoadPicture.Name = "toolStripButtonLoadPicture";
+            this.toolStripButtonLoadPicture.Size = new System.Drawing.Size(62, 22);
+            this.toolStripButtonLoadPicture.Text = "Картинка";
+            this.toolStripButtonLoadPicture.Click += new System.EventHandler(this.ToolStripButtonLoadPicture_Click);
+            // 
+            // toolStripButtonMixed
+            // 
+            this.toolStripButtonMixed.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonMixed.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonMixed.Name = "toolStripButtonMixed";
+            this.toolStripButtonMixed.Size = new System.Drawing.Size(82, 22);
+            this.toolStripButtonMixed.Text = "Перемешать";
+            this.toolStripButtonMixed.Click += new System.EventHandler(this.ToolStripButtonMixed_Click);
+            // 
+            // toolStripButtonRestore
+            // 
+            this.toolStripButtonRestore.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonRestore.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonRestore.Name = "toolStripButtonRestore";
+            this.toolStripButtonRestore.Size = new System.Drawing.Size(86, 22);
+            this.toolStripButtonRestore.Text = "Восстановить";
+            this.toolStripButtonRestore.Click += new System.EventHandler(this.ToolStripButtonRestore_Click);                        
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(640, 505);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FormMain";
+            this.Text = "Мозаика";
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonLoadPicture;
+        private System.Windows.Forms.ToolStripButton toolStripButtonMixed;
+        private System.Windows.Forms.ToolStripButton toolStripButtonRestore;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSetting;
+        private System.Windows.Forms.ToolStripButton toolStripButtonHelp;
+
+
+        #endregion
+    }
+}
+
